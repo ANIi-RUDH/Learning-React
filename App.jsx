18c1d83ff5acad726dac031e3b69847fdd813ae7
ANI-RUDH <117719175+ANIi-RUDH@users.noreply.github.com>
2024-08-20 09:08:58 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Head from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";  
+
+
+export default function APP(){
+return (
+<div>
+<Head/>
+<Note/>
+<Footer/>
+</div>)
+
+}
